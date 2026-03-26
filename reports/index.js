9ce04d9c9f4368b514aba1bee718dfaa5039f81f
Chandler Mayo <chandler.mayo@redpanda.com>
2026-03-26 10:05:30 -0400

    report: 2026-03-26T14-05-30Z

--- reports/index.js
+++ reports/index.js
@@ -1,6 +1,11 @@
 window.__devrelIndex={
   "reports": [
     {
+      "id": "2026-03-26T14-05-30Z",
+      "file": "2026-03-26T14-05-30Z.json",
+      "generated_at": "2026-03-26T14:05:30Z"
+    },
+    {
       "id": "2026-03-26T14-03-14Z",
       "file": "2026-03-26T14-03-14Z.json",
       "generated_at": "2026-03-26T14:03:14Z"
